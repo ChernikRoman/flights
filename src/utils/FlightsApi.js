f0781cd9ae1e9999efa31a6cf583612c56585e8c
Roman Chernik <chernik.r@yandex.ru>
2022-04-28 19:32:08 +0300

    feat: add FlightsApi util

---
+++ src/utils/FlightsApi.js
@@ -0,0 +1,27 @@
+class FlightsApi {
+  constructor({baseURL}) {
+      this.baseURL = baseURL;
+  }
+
+  _checkResponse(response) {
+      if(response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    }
+
+  getFlights() {
+      return fetch(`${this.baseURL}`, {
+          method: 'GET',
+      })
+        .then(this._checkResponse)
+  }
+}
+
+const flightsApi = new FlightsApi({
+  // baseURL: 'https://api.moredomains.nomoredomains.rocks/flights',
+  baseURL: 'http://localhost:3000/flights',
+});
+
+export default flightsApi;
