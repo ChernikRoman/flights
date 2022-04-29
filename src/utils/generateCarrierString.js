ae7dfb0527da7740b355de3230db1183a33a95f8
Roman Chernik <chernik.r@yandex.ru>
2022-04-29 13:40:44 +0300

    feat: add genereteCarrierString util

--- src/utils/generateCarrierString.js
+++ src/utils/generateCarrierString.js
@@ -1,3 +1,3 @@
 export default function generateCarrierString(carrier1, carrier2) {
-  return carrier1 === carrier2 ? carrier1 : `${carrier1} и ${carrier2}`;
+  return carrier1 === carrier2 ? ` ${carrier1}` : ` ${carrier1} и ${carrier2}`;
 }
