596e55218288d7b618d87d08851306e1d7006fd1
Roman Chernik <chernik.r@yandex.ru>
2022-04-29 13:42:36 +0300

    fix: zero in timeConverter

--- src/utils/timeConverter.js
+++ src/utils/timeConverter.js
@@ -1,5 +1,5 @@
 export default function timeConverter(time) {
   let hours = Math.trunc(time/60);
   let minutes = time % 60;
-  return `${hours} ч ${minutes} м`;
+  return `${hours < 10 ? '0' + hours : hours} ч ${minutes < 10 ? '0' + minutes : minutes} мин`;
 }
