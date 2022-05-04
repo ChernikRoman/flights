12f3bcdde5c5d0ed141b4ce8752425e0683daec0
Roman Chernik <chernik.r@yandex.ru>
2022-05-04 03:52:13 +0300

    refactor: change className in FLightsSort

--- src/components/FlightsSort/FlightsSort.js
+++ src/components/FlightsSort/FlightsSort.js
@@ -1,32 +1,25 @@
-import { useEffect, useState } from 'react';
 import './FlightsSort.css';
 
 export default function FlightsSort(props) {
-  const [sortBy, setSortBy] = useState('sortByMinPrice')
-
   function changeFormHandler(evt) {
     props.onSortHandler(evt.target.value)
   }
-
-  useEffect(()=>{
-    props.onSortHandler('sortByMinPrice')
-  }, [])
 
   return(
       <form className="flights-sort" onChange={(evt) => changeFormHandler(evt)}>
           <span className="flights-sort__description">
               Сортровка
           </span>
-          <label className="radio-button">
-              <input className="radio-button__input" type="radio" name="flights-sort" value="sortByMinPrice" defaultChecked={true}/>
+          <label className="sort-button">
+              <input className="sort-button__input" type="radio" name="flights-sort" value="sortByMinPrice" defaultChecked/>
               - по возрастанию цены
           </label>
-          <label className="radio-button">
-              <input className="radio-button__input" type="radio" name="flights-sort" value="sortByMaxPrice"/>
+          <label className="sort-button">
+              <input className="sort-button__input" type="radio" name="flights-sort" value="sortByMaxPrice"/>
               - по убыванию цене
           </label>
-          <label className="radio-button">
-              <input className="radio-button__input" type="radio" name="flights-sort" value="sortByDuration"/>
+          <label className="sort-button">
+              <input className="sort-button__input" type="radio" name="flights-sort" value="sortByDuration"/>
               - по времени в пути
           </label>
       </form>
